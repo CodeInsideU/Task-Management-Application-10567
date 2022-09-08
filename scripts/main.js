f099b66aa0a32da8bcb6ba61854929a24976cc5b
Vratik Zade <83591308+CodeInsideU@users.noreply.github.com>
2022-09-08 09:25:29 +0530

    done 2

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,4 @@
+
 const state = {
     taskList: [],
 };
@@ -8,6 +9,7 @@ const taskModal = document.querySelector(".task__modal__body");
 
 const htmlTaskContent = ({ id, title, description, type, url }) => `
     <div class='col-md-6 col-lg-4 mt-3' id=${id} key=${id}>
+
     <div class='card shadow-sm task__card'>
         <div class='card-header d-flex gap-2 justify-content-end task__card__header'>
         <button type='button' class='btn btn-outline-info mr-2' name=${id} onclick="editTask.apply(this, arguments)">
@@ -18,20 +20,24 @@ const htmlTaskContent = ({ id, title, description, type, url }) => `
         </button>
         </div>
         <div class='card-body'>
+
         ${url
         ? `<img width='100%' height='150px' style="object-fit: cover; object-position: center"  src=${url} alt='card image cap' class='card-image-top md-3 rounded-lg' />`
         : `
         <img width='100%' height='150px' style="object-fit: cover; object-position: center" src="https://reactnativecode.com/wp-content/uploads/2018/02/Default_Image_Thumbnail.png" alt='card image cap' class='img-fluid place__holder__image mb-3' />
         `
     }
+
         <h4 class='task__card__title'>${title}</h4>
         <p class='description trim-3-lines text-muted' data-gram_editor='false'>
             ${description}
         </p>
+
         <div class='tags text-white d-flex flex-wrap'>
             <span class='badge bg-primary m-1'>${type}</span>
         </div>
         </div>
+
         <div class='card-footer'>
         <button 
         type='button' 
